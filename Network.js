9a0e08d2e724c1460f2ea691317a7dc14634e93c
Pntbr <pntbr@scopyleft.fr>
2016-08-28 00:39:09 +0200

    Ajouter un feu

---
+++ Network.js
@@ -0,0 +1,10 @@
+class Network {
+	constructor() {
+		this.trafficLights = []
+	}
+	addTrafficLight(trafficLight) {
+		this.trafficLights.push(trafficLight)
+	}
+}
+
+const PomPomGaly = new Network()
