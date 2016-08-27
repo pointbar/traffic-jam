0506f5916429547a53f6e74187905a922b5735be
Pntbr <pntbr@scopyleft.fr>
2016-08-28 00:39:44 +0200

    Lister les feux

--- Network.js
+++ Network.js
@@ -5,6 +5,9 @@ class Network {
 	addTrafficLight(trafficLight) {
 		this.trafficLights.push(trafficLight)
 	}
+	listTrafficLight() {
+		return this.trafficLights.map(tl => tl.name)
+	}
 }
 
 const PomPomGaly = new Network()
